71543e47abb850e8acb462094b350ecf91c8f4e0
Stulcy <rokkstular@gmail.com>
2022-12-03 10:34:52 +0000

    Day 3

---
+++ day_3/day_3.dart
@@ -0,0 +1,41 @@
+import 'dart:io';
+
+void main() async {
+  String file = await File('day_3/large_input.txt').readAsString();
+  solve(file);
+}
+
+void solve(String file) {
+  final List<String> list = file.split('\n');
+  Set<String> firstHalfChars;
+  Set<String> secondHalfChars;
+  int result1 = 0;
+  int result2 = 0;
+  int counter = 0;
+  List<Set<String>> elfGroup = <Set<String>>[];
+  for (final String line in list) {
+    final List<String> lineList = line.split('');
+    firstHalfChars = lineList.take((line.length / 2).round()).toSet();
+    secondHalfChars = lineList.reversed
+        .take((line.length / 2).round())
+        .toList()
+        .reversed
+        .toSet();
+    final String common = firstHalfChars.intersection(secondHalfChars).first;
+    result1 += (common.toUpperCase() == common)
+        ? common.codeUnits.first - 38
+        : common.codeUnits.first - 96;
+    elfGroup.add(lineList.toSet());
+    if (counter % 3 == 2) {
+      final String common =
+          elfGroup[0].intersection(elfGroup[1]).intersection(elfGroup[2]).first;
+      elfGroup.clear();
+      result2 += (common.toUpperCase() == common)
+          ? common.codeUnits.first - 38
+          : common.codeUnits.first - 96;
+    }
+    counter++;
+  }
+  print('Part 1: $result1');
+  print('Part 2: $result2');
+}
